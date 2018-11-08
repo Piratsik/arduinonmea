2c1ca8bb1bc7a5aaecd6b96744065e42b43d0924
Piratsik <piratsik@gmail.com>
2018-11-08 20:33:40 +0200

    initial1

---
+++ sketch_nov08a.cpp
@@ -0,0 +1,26 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(10,11); // Rx,Tx
+
+#include <nmea.h>
+
+
+  NMEA nmeaDecoder(ALL);   
+     
+  void setup() {   
+  Serial.begin(9600);    
+  Serial.println("Goodnight moon!");
+
+  // set the data rate for the SoftwareSerial port
+mySerial.begin(9600);
+  }   
+     
+  void loop() {   
+  if (mySerial.available()) {                 // if something is incoming through the Serial Port
+    if (nmeaDecoder.decode(mySerial.read())) { // if it's a valid NMEA sentence   
+    Serial.println(nmeaDecoder.sentence());  // print it 
+
+    }
+  
+   }
+  
+  }
