0e9d3ff17755fefdc0becf4a329717d5392aa167
Spyros Sellas <s.sellas@hotmail.gr>
2018-11-08 20:43:03 +0200

    gamw ta tabs

--- sketch_nov08a.cpp
+++ sketch_nov08a.cpp
@@ -3,24 +3,26 @@ SoftwareSerial mySerial(10,11); // Rx,Tx
 
 #include <nmea.h>
 
+  NMEA nmeaDecoder(ALL);
 
-  NMEA nmeaDecoder(ALL);   
-     
-  void setup() {   
-  Serial.begin(9600);    
-  Serial.println("Goodnight moon!");
+  void setup() {
+    Serial.begin(9600);
+    Serial.println("Goodnight moon!");
 
-  // set the data rate for the SoftwareSerial port
-mySerial.begin(9600);
-  }   
-     
+    // set the data rate for the SoftwareSerial port
+    mySerial.begin(9600);
+  }
+
   void loop() {   
-  if (mySerial.available()) {                 // if something is incoming through the Serial Port
-    if (nmeaDecoder.decode(mySerial.read())) { // if it's a valid NMEA sentence   
-    Serial.println(nmeaDecoder.sentence());  // print it 
+    if (mySerial.available()) { // if something is incoming through the Serial Port
+      if (nmeaDecoder.decode(mySerial.read())) { // if it's a valid NMEA sentence   
+        Serial.println(nmeaDecoder.sentence()); // print it 
 
+        
+      } 
     }
-  
-   }
-  
+  }
+
+  void changeBPS(){
+
   }
