cdf4ac62e7d16d0c967d32574851f8fa98723e47
Piratsik <piratsik@gmail.com>
2018-11-08 20:47:14 +0200

    poutsakwlosmouni

--- sketch_nov08a.cpp
+++ sketch_nov08a.cpp
@@ -2,10 +2,11 @@
 SoftwareSerial mySerial(10,11); // Rx,Tx
 
 #include <nmea.h>
-
-  NMEA nmeaDecoder(ALL);
+NMEA nmeaDecoder(ALL);
+int button=9
 
   void setup() {
+	pinMode(button, INPUT); 
     Serial.begin(9600);
     Serial.println("Goodnight moon!");
 
@@ -17,8 +18,12 @@ SoftwareSerial mySerial(10,11); // Rx,Tx
     if (mySerial.available()) { // if something is incoming through the Serial Port
       if (nmeaDecoder.decode(mySerial.read())) { // if it's a valid NMEA sentence   
         Serial.println(nmeaDecoder.sentence()); // print it 
+		int state = digitalRead(button);
+        //check if button is pressed and it is over 5 seconds since last button press
+        if(state == HIGH && (millis() - changeTime)> 5000){
+               //call the function to change the baudrate
 
-        
+        }
       } 
     }
   }
